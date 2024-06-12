6538d11b205814af42c3d5b7a1f09844d065b221
Martin <68254461+littlee5521@users.noreply.github.com>
2024-06-12 11:55:04 -0700

    save

--- app/ui/landingpage/hero.tsx
+++ app/ui/landingpage/hero.tsx
@@ -5,7 +5,9 @@ import Link from "next/link";
 
 export default function LandingHero() {
   return (
-    <section className={`  text-center px-6 block sm:flex py-12 `}>
+    <section
+      className={`  text-center px-6 block sm:flex pt-2 sm:pt-12 py-12 `}
+    >
       <div
         className={` ${poppins.className} flex justify-between py-14 gap-5 sm:gap-0 flex-col`}
       >
@@ -16,17 +18,17 @@ export default function LandingHero() {
         <h1 className={` ${josefin_sans.className} text-5xl font-bold`}>
           Organize Your Cold Call Leads
         </h1>
-        <h2 className={` ${kanit.className} text-2xl`}>
+        <h2 className={` ${kanit.className} text-3xl`}>
           Effortlessly manage and track your cold call contacts and follow-ups
         </h2>
         {
           //this will be a text blurb
         }
-        <p className={` ${kanit.className} text-2xl`}>
+        <p className={` ${kanit.className} text-3xl`}>
           Simplify your outreach process with powerful tools to organize, track,
           and follow up with your contacts.
         </p>
-        <p className={` ${kanit.className} text-2xl`}>
+        <p className={` ${kanit.className} text-3xl`}>
           Stay on top of your communication with timely reminders and a
           streamlined interface.
         </p>
@@ -34,13 +36,13 @@ export default function LandingHero() {
           className={` ${kanit.className} w-fit mx-auto`}
           href={"/dashboard"}
         >
-          <p className="text-2xl w-fit py-2 px-4 transform transition-transform duration-300 hover:shadow-lg hover:-translate-y-1 shadow-md bg-cch_primary rounded-md m-auto">
+          <p className="text-3xl w-fit py-2 px-4 transform transition-transform duration-300 hover:shadow-lg hover:-translate-y-1 shadow-md bg-cch_primary rounded-md m-auto">
             Get Started
           </p>
         </Link>
       </div>
-      <div className="">
-        <Image alt="text" src={HeroImg} />
+      <div className=" flex flex-col justify-center">
+        <Image className="" alt="text" src={HeroImg} />
       </div>
     </section>
   );
